463c4135c6ca6cd5eeeb43b5d5254a4b8af43de5
Yeonjinwook <duswlsdnr1@nate.com>
2019-10-14 21:10:07 +0900

    [master eded] study

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //두 수를 입력
+            int a, b;
+            Console.WriteLine("첫 번째 수:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("두 번째 수:");
+            b = int.Parse(Console.ReadLine());
+            //큰 수와 작은 수를 판별
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            int sum = 0;
+            for (int i = a; i <= b; i++)//반복( i를 작은수에서 큰 수까지 순차적 증가)
+            {
+                if ((i % 5) == 0)
+                {
+                    sum = sum + i;//합계에 i를 더한다.
+
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+    
---
+++ grade_yjh/grade_yjh/20153011_유진환.cs
@@ -0,0 +1,66 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grade_yjh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score, average=0, i=0, sum=0;
+            char grade;
+            string answer;
+
+            first:
+
+            Console.Write("점수를 입력해주세요. : ");
+            score = int.Parse(Console.ReadLine());
+            if (score > 100 || score < 0)
+            {
+                Console.Write("잘못된 입력입니다.\n");
+                goto first;
+            }
+
+            switch (average)
+            {
+                case 10:
+                    grade = 'A';
+                    break;
+                case 9:
+                    grade = 'A';
+                    break;
+                case 8:
+                    grade = 'B';
+                    break;
+                case 7:
+                    grade = 'C';
+                    break;
+                case 6:
+                    grade = 'D';
+                    break;
+                default:
+                    grade = 'F';
+                    break;
+            }
+
+            Console.WriteLine("당신의 학점은 {0}입니다.", grade);
+
+            Console.Write("계속 하시겠습니까?\ny/n\n");
+            answer = Console.ReadLine();
+            if(answer == "y")
+            {
+                goto first;
+            }
+            else
+            {
+                return;
+            }
+
+           
+
+        }
+    }
+}
